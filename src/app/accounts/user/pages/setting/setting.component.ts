f4ebd691cbbaeb31510f17e4c8b525564f9ee7a8
JosephKWarteng1 <117072095+JosephKwarteng1@users.noreply.github.com>
2023-12-28 12:08:39 +0100

    implement user profile

--- src/app/accounts/user/pages/setting/setting.component.ts
+++ src/app/accounts/user/pages/setting/setting.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SideNavComponent } from '../../../../shared/components/side-nav/side-nav.component';
 import { HeaderComponent } from '../../../../shared/components/header/header.component';
+import { UserProfile } from '../../components/user-profile/user-profile.component';
 
 @Component({
   selector: 'setting',
   standalone: true,
-  imports: [CommonModule, SideNavComponent, HeaderComponent],
+  imports: [CommonModule, SideNavComponent, HeaderComponent, UserProfile],
   templateUrl: './setting.component.html',
   styleUrl: './setting.component.css',
 })
