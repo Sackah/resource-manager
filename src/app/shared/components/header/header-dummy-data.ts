a38bcf204b2e7b0f9edb20b94843a348d5689e33
JosephKWarteng1 <117072095+JosephKwarteng1@users.noreply.github.com>
2023-12-29 17:09:12 +0100

    implement notification drop down

---
+++ src/app/shared/components/header/header-dummy-data.ts
@@ -0,0 +1,26 @@
+export const Notifications = [
+  {
+    icon: 'avatar-2.svg',
+    from: 'Admin 2 mins ago',
+    description: 'Invited Alisa Hester to the project',
+    isHighlighted: true,
+  },
+  {
+    icon: 'avatar-3.svg',
+    from: 'Lana Steiner 2 mins ago',
+    description: 'Assigned Nexum as Client',
+    isHighlighted: true,
+  },
+  {
+    icon: 'avatar-4.svg',
+    from: 'Ben Moore 2 mins ago',
+    description: 'Invited Alisa Hester to the team',
+    isHighlighted: true,
+  },
+  {
+    icon: 'avatar-5.svg',
+    from: 'Lana Steiner 2 mins ago',
+    description: 'Invited Alisa Hester to the team',
+    isHighlighted: true,
+  },
+];
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,16 +2,20 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SearchComponent } from '../search/search.component';
+import { Notifications } from './header-dummy-data';
+import { CdkMenuModule } from '@angular/cdk/menu';
 
 @Component({
   selector: 'header',
   standalone: true,
-  imports: [CommonModule, FormsModule, SearchComponent],
+  imports: [CommonModule, FormsModule, SearchComponent, CdkMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   query: string = '';
+
+  notifications = Notifications;
 
   performSearch() {}
 }
