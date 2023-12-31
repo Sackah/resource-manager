64773d13f5b94c88bb7ab41182a4edd62e47446b
JosephKWarteng1 <117072095+JosephKwarteng1@users.noreply.github.com>
2023-12-31 01:03:46 +0100

    implement event listener for online/offline changes and respective indicators to show

--- src/app/accounts/user/pages/users/users.component.ts
+++ src/app/accounts/user/pages/users/users.component.ts
@@ -2,12 +2,23 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SideNavComponent } from '../../../../shared/components/side-nav/side-nav.component';
 import { HeaderComponent } from '../../../../shared/components/header/header.component';
+import { ButtonAssignComponent } from '../../components/button-assign/button-assign.component';
+import { ButtonNewComponent } from '../../components/button-new/button-new.component';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, SideNavComponent, HeaderComponent],
+  imports: [
+    CommonModule,
+    SideNavComponent,
+    HeaderComponent,
+    ButtonAssignComponent,
+    ButtonNewComponent,
+  ],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css',
+  styleUrls: [
+    './users.component.css',
+    '../../pages/setting/setting.component.css',
+  ],
 })
 export class UsersComponent {}
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SearchComponent } from '../search/search.component';
 import { Notifications } from './header-dummy-data';
@@ -12,10 +12,19 @@ import { CdkMenuModule } from '@angular/cdk/menu';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   query: string = '';
-
   notifications = Notifications;
+  isOnline: boolean = navigator.onLine;
+
+  ngOnInit() {
+    window.addEventListener('online', () => this.updateOnlineStatus(true));
+    window.addEventListener('offline', () => this.updateOnlineStatus(false));
+  }
+
+  private updateOnlineStatus(online: boolean) {
+    this.isOnline = online;
+  }
 
   performSearch() {}
 }
